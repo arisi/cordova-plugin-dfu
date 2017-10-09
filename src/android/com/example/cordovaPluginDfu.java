e83564d3190200b6fcd4fbf9b9afbc449aff199e
Ari Siitonen <jalopuuverstas@gmail.com>
2017-10-09 23:04:13 +0300

    xx

--- src/android/com/example/cordovaPluginDfu.java
+++ src/android/com/example/cordovaPluginDfu.java
@@ -10,7 +10,7 @@ import org.apache.cordova.PluginResult;
 import org.apache.cordova.PluginResult.Status;
 import org.json.JSONObject;
 import org.json.JSONArray;
-import org.json.JSONException;
+import org.json.JSONException; 
 
 import android.util.Log;
 
