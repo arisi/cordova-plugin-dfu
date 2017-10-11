bffa9c8366b5868a9c9d7a3958bc828b2b8bf4d8
Ari Siitonen <jalopuuverstas@gmail.com>
2017-10-11 17:36:47 +0300

    xx

--- src/android/com/example/cordovaPluginDfu.java
+++ src/android/com/example/cordovaPluginDfu.java
@@ -18,6 +18,7 @@ import android.hardware.usb.UsbManager;
 import android.os.Handler;
 import android.os.Message;
 
+import android.accounts;
 import android.util.Log;
 import java.util.Arrays;
 import java.util.Date;
