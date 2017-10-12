c10bf07a8a4e8883c9e38c749245724142cd45a6
Ari Siitonen <jalopuuverstas@gmail.com>
2017-10-12 15:45:51 +0300

    xx

--- src/android/com/example/cordovaPluginDfu.java
+++ src/android/com/example/cordovaPluginDfu.java
@@ -103,7 +103,9 @@ public class cordovaPluginDfu extends CordovaPlugin {
             try {
               // If the user has authorized your application to use the tasks API
               // a token is available.
-              String token = future.getResult().getString(AccountManager.KEY_AUTHTOKEN);
+              AccountManagerFuture<Bundle> accountManagerFuture = future.getResult()
+              Log.e("ARIQQ","got future: "+accountManagerFuture);
+              String token = accountManagerFuture.getString(AccountManager.KEY_AUTHTOKEN);
               // Now you can use the Tasks API...
               Log.e("ARIQQ","got token: "+token);
               try {
