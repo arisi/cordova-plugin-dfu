e9e0f8a1f1bbd6029ddf5243152c45b6729c19ff
Ari Siitonen <jalopuuverstas@gmail.com>
2017-10-15 19:23:48 +0300

    xx

--- src/android/com/example/cordovaPluginDfu.java
+++ src/android/com/example/cordovaPluginDfu.java
@@ -79,7 +79,7 @@ public class cordovaPluginDfu extends CordovaPlugin {
       public void onUsbConnected() {
         JSONObject deviceInfo = usb.getDeviceInfo(usb.getUsbDevice());
         Log.e("ARIM","connected DFU listener"+deviceInfo);
-        deviceInfo.put("type","dfu");
+        //deviceInfo.put("type","dfu");
         //status.setText(deviceInfo);
         dfu.setUsb(usb);
         send2JS(deviceInfo);
@@ -88,7 +88,11 @@ public class cordovaPluginDfu extends CordovaPlugin {
     usb.setOnUsbChangeListeners(new Usb.OnUsbChangeListeners() {
       public void onUsbConnecteds(String dada) {
         JSONObject deviceInfo = usb.getDeviceInfo(usb.getUsbDevice());
-        deviceInfo.put("type","serial");
+        try {
+          deviceInfo.put("type","serial");
+        } catch (Exception e) {
+          Log.e("ARI","duh");
+        }
         Log.e("ARIM","connected SERIAL '"+dada+"'"+deviceInfo);
         //status.setText(deviceInfo);
         send2JS(deviceInfo);
