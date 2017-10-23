434c2b2339bb3da761a3ea21a9dd6617ba234d98
Ari Siitonen <jalopuuverstas@gmail.com>
2017-10-23 19:16:25 +0300

    x

--- src/android/com/example/Usb.java
+++ src/android/com/example/Usb.java
@@ -232,13 +232,24 @@ public class Usb {
       }
     }
 
+    private final static char[] hexArray = "0123456789ABCDEF".toCharArray();
+    public static String bytesToHex(byte[] bytes) {
+        char[] hexChars = new char[bytes.length * 2];
+        for ( int j = 0; j < bytes.length; j++ ) {
+            int v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+
     private UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback() {
         @Override
         public void onReceivedData(byte[] arg0) {
             try {
                 String data = new String(arg0, "UTF-8");
                 byte[] bytes = data.getBytes();
-                Log.e("ARIS","got raw: "+bytes);
+                Log.e("ARIS","got raw: "+bytesToHex(bytes));
                 String s="";
                 for (int i=0;i<bytes.length;i++) {
                   s=s+String.format("%02X ", bytes[i]);
